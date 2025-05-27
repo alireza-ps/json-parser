dd71b666fd6edcac58b712bb71d3e67ddcfe755e
alireza <alirezatl135@gmail.com>
2025-05-27 20:00:13 +0330

    convert value to target type

--- src/main/java/com/github/alirezaps/jsonserializer/deserializer/JsonDeserializer.java
+++ src/main/java/com/github/alirezaps/jsonserializer/deserializer/JsonDeserializer.java
@@ -14,9 +14,34 @@ public class JsonDeserializer {
         for (Field field : fields) {
             field.setAccessible(true);
             Object value = map.get(field.getName());
-            field.set(instance, value);
+            Class<?> targetType = field.getType();
+            Object obj = convertType(value, targetType);
+            field.set(instance, obj);
         }
         return instance;
 
+    }
+
+    private Object convertType(Object value, Class<?> targetType) {
+        if (value == null) return null;
+        if (targetType == byte.class || targetType == Byte.class)
+            return ((Number) value).byteValue();
+        else if (targetType == short.class || targetType == Short.class)
+            return ((Number) value).shortValue();
+        else if (targetType == int.class || targetType == Integer.class)
+            return ((Number) value).intValue();
+        else if (targetType == long.class || targetType == Long.class)
+            return ((Number) value).longValue();
+        else if (targetType == double.class || targetType == Double.class)
+            return ((Number) value).doubleValue();
+        else if (targetType == float.class || targetType == Float.class)
+            return ((Number) value).floatValue();
+        else if (targetType == boolean.class || targetType == Boolean.class)
+            return value instanceof Boolean ? value : Boolean.parseBoolean(value.toString());
+        else if (targetType == String.class)
+            return value.toString();
+        else if (targetType == Enum.class)
+            return Enum.valueOf((Class<Enum>) targetType, value.toString());
+        throw new RuntimeException("Unsupported targetType.");
     }
 }
